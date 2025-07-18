44eb144df6cd7b3f6bb0826d4e8902f46f566603
Adamya507 <Adamyapareek.12316@gmail.com>
2025-07-18 15:13:21 +0530

    Add files via upload

---
+++ Number.guessing.game.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+//NUMBER GUESSING GAME
+
+int main() {
+    int num = 13;
+    int n, i = 0;
+    cout<<"Let the game begin 🎯 "<<endl;
+    cout << "Guess the number : " << endl;
+
+    do {
+        cin >> n;
+        if (n == num) {
+            cout << "correct 🎉 ";
+            break;
+        }
+        else if (n > num) {
+            cout << "guess lower";
+        }
+        else {
+            cout << "guess higher";
+        }
+        cout << endl;
+    } while (i >= 0);
+    i++;
+    return 0;
+}
